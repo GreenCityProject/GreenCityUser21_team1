12e40c68ff39c59eb950ec1eeda4767a3ce4a988
loshkarjova <loshkarjova@gmail.com>
2024-03-06 21:12:00 +0000

    fixed

--- service/src/main/java/greencity/service/EmailServiceImpl.java
+++ service/src/main/java/greencity/service/EmailServiceImpl.java
@@ -245,7 +245,7 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendHabitNotification(String name, String email) {
-        userRepo.findByEmail(email).orElseThrow(() -> new EmailNotVerified("Email " + email + " is not found"));
+        userRepo.findByEmail(email).orElseThrow(() -> new NotFoundException("Email " + email + " is not found"));
         String subject = "Notification about not marked habits";
         String content = "Dear " + name + ", you haven't marked any habit during last 3 days";
         sendEmail(email, subject, content);
